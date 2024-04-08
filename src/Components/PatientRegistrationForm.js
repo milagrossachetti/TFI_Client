f0de535d44adcac7cc1add38bbd290f258059f24
Milagros Sachetti <milagrossachett@gmail.com>
2024-04-08 01:51:32 -0300

    Le agregué un poco de css

--- src/Components/PatientRegistrationForm.js
+++ src/Components/PatientRegistrationForm.js
@@ -2,11 +2,14 @@ import { data } from 'autoprefixer';
 import './Components.css';
 
 import React, { useEffect, useState } from "react";
-import { useForm, useFieldArray, set } from "react-hook-form";
-import { v4 as uuidv4 } from 'uuid';
+import { useForm, useFieldArray } from "react-hook-form";
+import PrimaryCard from './PrimaryCard';
+import SecondaryCard from './SecondaryCard';
+import RequiredFieldsLabel from './RequiredFieldsLabel'
 
 function PatientRegistrationForm() {
-    
+    //para obtener las preguntas a partir del get
+    const[listQuestion, setListQuestions] = useState([]);
     const {register, handleSubmit, control} = useForm();
     const onSubmit = handleSubmit((data) => {
         const filteredAnswers = data.answers.filter(answer => answer !== null);
@@ -34,10 +37,7 @@ function PatientRegistrationForm() {
             .then(data => console.log(data))
             .catch(error => console.error(error));
     });
-
-    //para obtener las preguntas a partir del get
-    const[listQuestion, setListQuestions] = useState([]);
-    const{remove} = useFieldArray({
+    const{} = useFieldArray({
         control,
         name: 'answers'  //array de objetos (lista de respuestas)
     })
@@ -53,45 +53,119 @@ function PatientRegistrationForm() {
             console.log(err.message);
         })
     }, []);
-    
-    return (
-        <form onSubmit={onSubmit}>
-            <label htmlFor='name'>Nombre*</label>
-            <input type='text' id='name' name='name' {...register('name', {required: true})}></input>
-            <label htmlFor='id'>DNI*</label>
-            <input type='text' id='id' {...register('id', {required: true})}></input>
-            <label htmlFor='birthday'>Fecha de nacimiento*</label>
-            <input type='date' id='birthday' {...register('birthday', {required: true})}></input>
-            <label htmlFor='phone'>Celular*</label>
-            <input type='tel' id='phone' {...register('phone', {required: true})}></input>
-            <label htmlFor='mail'>Correo electrónico*</label>
-            <input type='email' id='mail' {...register('mail', {required: true})}></input>
-            <label htmlFor='sex' >Sexo*</label>
-            <input type='text' id='sex' {...register('sex', {required: true})}></input>
-            <label htmlFor='age'>Edad*</label>
-            <input type='text' id='age' {...register('age', {required: true})}></input>
-            <label htmlFor='address'>Dirección*</label>
-            <input ttype='text' id='address' {...register('address', {required: true})}></input>
-            <label htmlFor='location'>Localidad*</label>
-            <input type='text' id='location' {...register('location', {required: true})}></input>
-            {listQuestion.map(question => (
-                <div key={question.id}>
-                    <label htmlFor={`question-${question.id}`}>{question.text}</label>
-                    {question.chosen ? (
-                        <div>
-                            <input type='checkbox' id={`question.${question.id}`} {...register(`answers.${question.id}.text`)}></input>
-                            <input type="hidden" id={`question.${question.id}`} value={question.id} {...register(`answers.${question.id}.question_id`)}/>
-                        </div>
-                        ) : <div>
-                                <input type='text' id={`question.${question.id}`} {...register(`answers.${question.id}.text`)}></input>
+    //const filteredAnswers = data.answers.filter(answer => answer !== null);
+    const firstQuestions = listQuestion.filter(question => question.id <= 4);
+    const remainingQuestions = listQuestion.filter(question => question.id > 4);
+    const FirstQuestionsComponent = ({questions}) => (
+        <>
+            <h4 className="font-montserrat text-sm text-secondary-color text-center mx-auto mt-4">Prótesis</h4>
+            {questions.map(question => (
+                <div key={question.id} className='grid grid-cols-2 gap-2 text-center'>
+                    <div className="flex items-center">
+                        <label htmlFor={`question-${question.id}`} className='font-montserrat text-sm text-main-color'>{question.text}</label>
+                    </div>
+                    <div className="flex items-center justify-end">
+                        {question.chosen ? (
+                            <div className='flex flex-col gap-2'>
+                                <input type='checkbox' id={`question.${question.id}`} {...register(`answers.${question.id}.text`)} className='accent-secondary-color focus:ring-0 h-5 w-5'></input>
                                 <input type="hidden" id={`question.${question.id}`} value={question.id} {...register(`answers.${question.id}.question_id`)}/>
                             </div>
-                        }
-                    
-                </div>)
-            )}
-            <button type="submit">Aceptar</button>
-        </form>
+                        ) : (
+                            <div>
+                                <input type='text' id={`question.${question.id}`} {...register(`answers.${question.id}.text`)} className="appearance-none bg-transparent border-b w-full font-montserrat text-sm text-main-color focus:outline-none"></input>
+                                <input type="hidden" id={`question.${question.id}`} value={question.id} {...register(`answers.${question.id}.question_id`)}/>
+                            </div>
+                        )}
+                    </div>
+                </div>  
+            ))}
+        </>
+    );
+    
+    const RemainingQuestionsComponent = ({questions}) => (
+        <>
+            {questions.map(question => (
+                <div key={question.id} className='grid grid-cols-2 gap-2 text-center'>
+                    <div className="flex items-center">
+                        <label htmlFor={`question-${question.id}`} className='font-montserrat text-sm text-main-color text-left'>{question.text}</label>
+                    </div>
+                    <div className="flex items-center justify-end">
+                        {question.chosen ? (
+                            <div className='flex flex-col gap-2'>
+                                <input type='checkbox' id={`question.${question.id}`} {...register(`answers.${question.id}.text`)} className='accent-secondary-color focus:ring-0 h-5 w-5'></input>
+                                <input type="hidden" id={`question.${question.id}`} value={question.id} {...register(`answers.${question.id}.question_id`)}/>
+                            </div>
+                        ) : (
+                            <div>
+                                <input type='text' id={`question.${question.id}`} {...register(`answers.${question.id}.text`)} className="appearance-none bg-transparent border-b w-full font-montserrat text-sm text-main-color focus:outline-none"></input>
+                                <input type="hidden" id={`question.${question.id}`} value={question.id} {...register(`answers.${question.id}.question_id`)}/>
+                            </div>
+                        )}
+                    </div>
+                </div>  
+            ))}
+        </>
+    );    
+    return (
+        <PrimaryCard>
+            <form onSubmit={onSubmit}>
+                <div className="grid grid-cols-2 grid-rows-2 gap-2">
+                    <div className='grid grid-cols-1 gap-2'>
+                        <h4 className="font-abel text-lg text-main-color">Añadir información del paciente</h4>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='name' className='font-montserrat text-base text-main-color'>Nombre*</label>
+                            <input type='text' id='name' name='name' {...register('name', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='id' className='font-montserrat text-base text-main-color'>DNI*</label>
+                            <input type='text' id='id' {...register('id', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='birthday' className='font-montserrat text-base text-main-color'>Fecha de nacimiento*</label>
+                            <input type='date' id='birthday' {...register('birthday', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='phone' className='font-montserrat text-base text-main-color'>Celular*</label>
+                            <input type='tel' id='phone' {...register('phone', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='mail' className='font-montserrat text-base text-main-color'>Correo electrónico*</label>
+                            <input type='email' id='mail' {...register('mail', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='sex' className='font-montserrat text-base text-main-color'>Sexo*</label>
+                            <input type='text' id='sex' {...register('sex', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='age' className='font-montserrat text-base text-main-color'>Edad*</label>
+                            <input type='text' id='age' {...register('age', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='address' className='font-montserrat text-base text-main-color'>Dirección*</label>
+                            <input ttype='text' id='address' {...register('address', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <div className='flex flex-row items-center gap-4'>
+                            <label htmlFor='location' className='font-montserrat text-base text-main-color'>Localidad*</label>
+                            <input type='text' id='location' {...register('location', {required: true})} className="appearance-none bg-transparent border-b w-full font-montserrat text-main-color focus:outline-none"></input>
+                        </div>
+                        <SecondaryCard>
+                            <FirstQuestionsComponent questions={firstQuestions}/>
+                        </SecondaryCard>
+                    </div>
+                    <SecondaryCard>
+                        <RemainingQuestionsComponent questions={remainingQuestions}/>
+                    </SecondaryCard>
+                </div>
+                <div className='flex items-center justify-between px-4 m-1'>
+                        <RequiredFieldsLabel>
+                            Los campos que contienen (*) son campos que tienen que completarse sin excepción
+                        </RequiredFieldsLabel>
+                        <div className='mr-4'>
+                            <button type="submit">Aceptar</button>
+                        </div>
+                </div>
+            </form>
+        </PrimaryCard>
     );
 }
 
--- src/Components/PrimaryCard.js
+++ src/Components/PrimaryCard.js
@@ -1,6 +1,6 @@
 function PrimaryCard({children}) {
     return (
-        <div className="bg-white shadow-[0_4px_10px_rgb(100,185,185,0.5)] rounded-2xl px-4 py-2 m-2">
+        <div className="bg-white shadow-[0_4px_10px_rgb(100,185,185,0.5)] rounded-2xl px-4 py-2 m-2 inline-block">
             {children}
         </div>
     )
